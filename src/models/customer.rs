80388216126c99e66eb700c565e018b59abe6a13
Viacheslav Matrizaev <matrizaev@gmail.com>
2025-11-07 14:30:29 +0300

    fixed tests

--- src/models/customer.rs
+++ src/models/customer.rs
@@ -12,11 +12,11 @@ pub struct Customer {
     pub id: i32,
     pub hub_id: i32,
     pub name: String,
-    pub email: Option<String>,
     pub price_level_id: Option<i32>,
     pub created_at: NaiveDateTime,
     pub updated_at: NaiveDateTime,
     pub phone: String,
+    pub email: Option<String>,
 }
 
 #[derive(Insertable)]
