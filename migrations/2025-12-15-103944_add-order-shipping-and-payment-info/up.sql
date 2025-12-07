d9aa9366e7bf0dada9daf88b189bfdfaa452664b
Viacheslav Matrizaev <matrizaev@gmail.com>
2025-12-07 13:38:10 +0300

    Added order info columns

---
+++ migrations/2025-12-15-103944_add-order-shipping-and-payment-info/up.sql
@@ -0,0 +1,4 @@
+ALTER TABLE orders ADD COLUMN shipping_address TEXT;
+ALTER TABLE orders ADD COLUMN consignee TEXT;
+ALTER TABLE orders ADD COLUMN delivery_notes TEXT;
+ALTER TABLE orders ADD COLUMN payer TEXT;
